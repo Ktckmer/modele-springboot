bd0b643226dfe7938e3889794989e6171f2590dc
Cedrick Kamsi <kamsicdrick@gmail.com>
2019-06-15 21:27:46 +0200

    initial commit

---
+++ src/main/java/org/sid/ModeleApplication.java
@@ -0,0 +1,13 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModeleApplication.class, args);
+    }
+
+}
